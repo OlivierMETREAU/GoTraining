ceda0a75975a51e324286db1d833c7fb524b1c1d
OlivierM74 <metreau.olivier@ikmail.com>
2026-02-07 15:41:14 +0100

    Add the possibility to delete task by Id

--- day01-todo-cli/tasks/tasks.go
+++ day01-todo-cli/tasks/tasks.go
@@ -1,5 +1,9 @@
 package tasks
 
+import (
+	"errors"
+)
+
 // Task state - can be Todo, InProgress or Done
 type TaskState int
 
@@ -36,11 +40,29 @@ func New() Tasks {
 }
 
 // Add a task to the task list
-func (tasks *Tasks) Add(description string) {
+func (tasks *Tasks) Add(description string) (int, error) {
+	if description == "" {
+		return -1, errors.New("Cannot add a task with an empty description.")
+	}
 	tasks.TaskList[tasks.TaskCount] = Task{
 		Description: description,
 		Id:          tasks.TaskCount,
 		State:       StateTodo,
 	}
 	tasks.TaskCount++
+	return tasks.TaskCount - 1, nil
+}
+
+func (tasks *Tasks) Delete(id int) error {
+	_, ok := tasks.TaskList[id]
+	if !ok {
+		return errors.New("Cannot find the task Id in the task list.")
+	}
+	delete(tasks.TaskList, id)
+	return nil
+}
+
+func (tasks *Tasks) IsPresent(id int) bool {
+	_, ok := tasks.TaskList[id]
+	return ok
 }
--- day01-todo-cli/tasks/tasks_test.go
+++ day01-todo-cli/tasks/tasks_test.go
@@ -27,3 +27,69 @@ func TestAddTaskAppendTheTaskList(t *testing.T) {
 	assert.Equal(t, StateTodo, tasks.TaskList[0].State, "State shall be toDo after task add.")
 	assert.Equal(t, expectedTaskDescription, tasks.TaskList[0].Description)
 }
+
+func TestAddTaskWithValidTaskReturnsNoError(t *testing.T) {
+	tasks := New()
+	expectedTaskDescription := "New task description."
+	i, err := tasks.Add(expectedTaskDescription)
+	assert.Equal(t, 0, i, "Add shall return no error when adding a valid task.")
+	assert.Equal(t, nil, err, "Add shall return no error when adding a valid task.")
+}
+
+func TestAddTaskWithNonValidTaskReturnsAnError(t *testing.T) {
+	tasks := New()
+	expectedTaskDescription := ""
+	i, err := tasks.Add(expectedTaskDescription)
+	assert.NotEqual(t, 0, i, "Add shall return an error when adding a non valid task.")
+	assert.NotEqual(t, nil, err, "Add shall return an error when adding a non valid task.")
+}
+
+func TestAddTaskWithNonValidTaskDoesNotAddAnyTask(t *testing.T) {
+	tasks := New()
+	expectedTaskDescription := ""
+	tasks.Add(expectedTaskDescription)
+	assert.Equal(t, 0, tasks.TaskCount, "Add shall not add a non valid task.")
+	assert.Empty(t, tasks.TaskList, "Add shall not add a non valid task.")
+}
+
+func TestAddingMultipleTasks(t *testing.T) {
+	tasks := New()
+	tasks.Add("First task")
+	tasks.Add("Second task")
+	assert.Equal(t, 2, tasks.TaskCount)
+	assert.Equal(t, 0, tasks.TaskList[0].Id)
+	assert.Equal(t, "First task", tasks.TaskList[0].Description)
+	assert.Equal(t, 1, tasks.TaskList[1].Id)
+	assert.Equal(t, "Second task", tasks.TaskList[1].Description)
+}
+
+func TestDeleteAnUnknownTaskReturnsAnError(t *testing.T) {
+	tasks := New()
+	err := tasks.Delete(0)
+	assert.NotEqual(t, nil, err)
+}
+
+func TestDeleteAKnownTaskReturnsNoError(t *testing.T) {
+	tasks := New()
+	tasks.Add("New Task")
+	err := tasks.Delete(0)
+	assert.Equal(t, nil, err)
+}
+
+func TestDeleteAKnownTaskRemoveTheTaskFromTheList(t *testing.T) {
+	tasks := New()
+	tasks.Add("New Task")
+	tasks.Delete(0)
+	assert.Empty(t, tasks.TaskList)
+}
+
+func TestDeleteAKnowTaskRemoveTheCorrectTask(t *testing.T) {
+	tasks := New()
+	tasks.Add("First task")
+	i, _ := tasks.Add("Second task")
+	tasks.Add("Third task")
+	tasks.Delete(i)
+	assert.Equal(t, true, tasks.IsPresent(0))
+	assert.Equal(t, false, tasks.IsPresent(1))
+	assert.Equal(t, true, tasks.IsPresent(2))
+}
