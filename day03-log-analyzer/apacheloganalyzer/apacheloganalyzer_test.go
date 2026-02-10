4a7a2c6d5515ef0773aa4a582a59f248470b509c
OlivierM74 <metreau.olivier@ikmail.com>
2026-02-10 15:51:22 +0100

    Fix TestAccessLine dur to change in function output.

--- day03-log-analyzer/apacheloganalyzer/apacheloganalyzer_test.go
+++ day03-log-analyzer/apacheloganalyzer/apacheloganalyzer_test.go
@@ -10,7 +10,7 @@ import (
 func TestAccessLine(t *testing.T) {
 	line := "83.149.9.216 - - [17/May/2015:10:05:03 +0000] \"GET /images/name.png HTTP/1.1\" 200 203023 \"http://exmaple.com/presentations/\" \"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1700.77 Safari/537.36\""
 	la := New()
-	al := la.DecodeLine(line)
+	al, _ := la.DecodeLine(line)
 	assert.Equal(t, "83.149.9.216", al.IP)
 	assert.Equal(t, "-", al.UserID)
 	assert.Equal(t, time.Date(2015, 05, 17, 10, 5, 3, 0, time.UTC), al.DateTime)
