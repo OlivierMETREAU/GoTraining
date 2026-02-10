f8d8b7e9fe7c7158fd2e8a8826e9a954d8fe353c
OlivierM74 <metreau.olivier@ikmail.com>
2026-02-10 15:50:00 +0100

    Create the ASCII histogram of request per hour of the day.

--- day03-log-analyzer/main.go
+++ day03-log-analyzer/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"os"
+	"sort"
+	"strconv"
 
 	apacheloganalyzer "example.com/day03-log-analyzer/apacheloganalyzer"
 	accesslog "example.com/day03-log-analyzer/loganalyzer"
@@ -15,6 +17,7 @@ func main() {
 	fmt.Println(la.Counters["Hours"])
 	mostFreqIp := getMostFrequentIP(la)
 	fmt.Printf("Most used IP is %s, is %d occurences\n", mostFreqIp, la.Counters["IP"][mostFreqIp])
+	generateAsciiHistogram(la)
 }
 
 func getMostFrequentIP(la accesslog.LogAnalyzer) string {
@@ -31,4 +34,43 @@ func getMostFrequentIP(la accesslog.LogAnalyzer) string {
 	}
 
 	return maxKey
+}
+
+func generateAsciiHistogram(la accesslog.LogAnalyzer) {
+	hours := make([]int, 0, len(la.Counters["Hours"]))
+	var histo = map[int]int{}
+	max := 0
+	for h := range la.Counters["Hours"] {
+		intHour, _ := strconv.Atoi(h)
+		histo[intHour] = la.Counters["Hours"][h]
+		hours = append(hours, intHour)
+		if la.Counters["Hours"][h] > max {
+			max = la.Counters["Hours"][h]
+		}
+	}
+	sort.Ints(hours)
+	divider := max / 10
+
+	for h := range histo {
+		histo[h] /= divider
+	}
+	max = 10
+
+	// Print histogram from top to bottom
+	for level := max; level > 0; level-- {
+		for _, h := range hours {
+			if histo[h] >= level {
+				fmt.Print(" █ ") // ASCII-safe block alternative: "#"
+			} else {
+				fmt.Print("   ")
+			}
+		}
+		fmt.Println()
+	}
+
+	// Print hour labels
+	for _, h := range hours {
+		fmt.Printf("%2d ", h)
+	}
+	fmt.Println()
 }
