d2a57bee2023282522ed5836ce756093bf02ba5e
OlivierM74 <metreau.olivier@ikmail.com>
2026-02-11 15:39:14 +0100

    Complete the main function with a fixed input list.

--- day05-concurrency-pipeline/main.go
+++ day05-concurrency-pipeline/main.go
@@ -4,6 +4,28 @@ import "fmt"
 
 func main() {
 	fmt.Println("day05-concurrency-pipeline")
+	numbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+	input := make(chan int)
+	step1 := FilterEvenNumbers(input)
+	step2 := MultiplyByThree(step1)
+	step3 := SumValues(step2)
+
+	go func() {
+		defer close(input)
+		for _, n := range numbers {
+			input <- n
+		}
+	}()
+
+	total := <-step3
+	fmt.Println("Input list : ")
+	fmt.Println(numbers)
+	fmt.Printf("The sum of the even numbers multiplied by 3 is %d\n", total)
+
+	// {1, 2, 3, 4, 5, 6, 7, 8, 9}
+	// step1: {2,4,6,8}
+	// step2: {6,12,18,24}
+	// step3: 60
 }
 
 func FilterEvenNumbers(in <-chan int) <-chan int {
